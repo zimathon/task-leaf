fb4d1b6d53d1a73910ab72bba884b5608bc4cbe5
Yusuke Sasajima <zimathon@gmail.com>
2019-03-18 00:27:56 +0900

    add login/logout feature

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,10 +1,11 @@
 class TasksController < ApplicationController
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
   def index
-    @tasks = Task.all
+    @tasks = current_user.tasks.older
   end
 
   def show
-    @task = Task.find(params[:id])
+    #@task = current_user.tasks.find(params[:id])
   end
 
   def new
@@ -12,34 +13,37 @@ class TasksController < ApplicationController
   end
 
   def edit
-    @task = Task.find(params[:id])
+    #@task = current_user.tasks.find(params[:id])
   end
 
   def update
-    task = Task.find(params[:id])
-    task.update!(task_params)
-    redirect_to tasks_url, notice: "edit task #{task.name}"
+    # task = current_user.tasks.find(params[:id])
+    @task.update!(task_params)
+    redirect_to tasks_url, notice: "edit task #{@task.name}"
   end
 
   def create
-    @task = Task.new(task_params)
+    @task = current_user.tasks.new(task_params)
 
     if @task.save
-      redirect_to tasks_url, notice: "add task #{task.name}"
+      redirect_to tasks_url, notice: "add task #{@task.name}"
     else
       render :new
     end
   end
 
   def destroy
-    task = Task.find(params[:id])
-    task.destroy
-    redirect_to tasks_url, notice: "delete task #{task.name}"
+    # task = current_user.tasks.find(params[:id])
+    @task.destroy
+    redirect_to tasks_url, notice: "delete task #{@task.name}"
   end
 
   private
 
   def task_params
     params.require(:task).permit(:name, :description)
+  end
+  def set_task
+    @task = current_user.tasks.find(params[:id])
   end
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -2,10 +2,14 @@ class Task < ApplicationRecord
   validates :name, presence: true, length: {maximum: 30}
   validate :validate_name_not_including_commma
 
+  belongs_to :user
+
+  scope :recent, -> { order(created_at: :desc)}
+  scope :older, -> { order(created_at: :asc)}
+
   private
 
   def validate_name_not_including_commma
     errors.add(:name, 'not include comma') if name&.include?(',')
   end
 end
-
