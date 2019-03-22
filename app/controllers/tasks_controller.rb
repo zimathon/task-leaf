5a59087e1eb63096af018782cd7e5d7af87e3e5b
Yusuke Sasajima <zimathon@gmail.com>
2019-03-22 13:37:50 +0900

    add: csv export feature

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -4,6 +4,11 @@ class TasksController < ApplicationController
     @q = current_user.tasks.ransack(params[:q])
     # @tasks = current_user.tasks.older
     @tasks = @q.result(distinct: true)
+
+    respond_to do |format|
+      format.html
+      format.csv {send_data @tasks.generate_csv, filename: "tasks-#{Time.zone.now.strftime('%Y%m%d%S')}.csv"}
+    end
   end
 
   def show
--- app/models/task.rb
+++ app/models/task.rb
@@ -15,6 +15,19 @@ class Task < ApplicationRecord
   scope :recent, -> { order(created_at: :desc)}
   scope :older, -> { order(created_at: :asc)}
 
+  def self.csv_attributes
+    ["name", "description", "created_at", "updated_at"]
+  end
+
+  def self.generate_csv
+    CSV.generate(headers: true) do  |csv|
+      csv << csv_attributes
+      all.each do |task|
+        csv << csv_attributes.map{|attr| task.send(attr)}
+      end
+    end
+  end
+
   private
 
   def validate_name_not_including_commma
