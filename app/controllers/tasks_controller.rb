c7c654904f26f7e23eed3808a3a7f00002f02a69
Yusuke Sasajima <zimathon@gmail.com>
2019-03-22 09:58:27 +0900

    add: task attachment image

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -54,7 +54,7 @@ class TasksController < ApplicationController
   private
 
   def task_params
-    params.require(:task).permit(:name, :description)
+    params.require(:task).permit(:name, :description, :image)
   end
   def set_task
     @task = current_user.tasks.find(params[:id])
